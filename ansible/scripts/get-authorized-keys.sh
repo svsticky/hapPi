9523f34c5afb40987d94eff6a0aab81df937d670
Maarten van den Berg <maartenberg1@gmail.com>
2018-11-13 22:47:27 +0100

    Use GitHub teams instead of org

--- ansible/scripts/get-authorized-keys.sh
+++ ansible/scripts/get-authorized-keys.sh
@@ -14,27 +14,31 @@
 # Unofficial Bash strict mode
 set -eEfuo pipefail
 
-ORG=svsticky
+TEAMS=(
+	534284 # IT Crowd
+	553592 # CommIT
+)
 
-if [ ! -f .github-secret ]; then
-  echo "No GitHub secret stored, run authorized-github.sh first!"
+if [[ ! -f .github-secret ]]; then
+  echo "No GitHub secret stored, run authorize-github.sh first!"
   exit 1
 fi
 
 echo "Creating .new-authorized-keys."
 echo "# {{ ansible_managed }}" > .new-authorized-keys
 
-echo "Retrieving users."
-USERS=$(curl -u $(cat .github-secret) https://api.github.com/orgs/$ORG/members | jq -r '.[].login')
+for team in ${TEAMS[@]}; do
+	echo "Retrieving users for team $team."
+	USERS=$(curl -sS -u $(cat .github-secret) https://api.github.com/teams/$team/members | jq -r '.[].login')
 
-for user in $USERS; do
-  echo "Retrieving keys for $user..."
-  echo "# Keys for $user" >> .new-authorized-keys
-  curl https://api.github.com/users/$user/keys | jq -r '.[].key' >> .new-authorized-keys
+	for user in $USERS; do
+	  echo "Retrieving keys for $user..."
+	  echo "# Keys for $user" >> .new-authorized-keys
+	  curl -sS https://api.github.com/users/$user/keys | jq -r '.[].key' >> .new-authorized-keys
+	done
 done
 
 echo "Updating authorized-keys template."
-
 mv --backup=numbered .new-authorized-keys templates/home/pi/.ssh/authorized_keys.j2
 
 echo "Done!"
